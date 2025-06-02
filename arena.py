574f50168faff001ef29c34ebfd07a136fd6eb48
carbood <168190155+Back370@users.noreply.github.com>
2025-06-03 00:53:01 +0900

    クライアントセットアップ完了

--- arena.py
+++ arena.py
@@ -4,19 +4,21 @@ if __name__ == "__main__":
     #
     # もし事前に定義したクライアントを渡したい場合:
     #
-    from client.sample_arena_client import PlusOneClient as PlusOneClient
-    from client.sample_arena_client import ConstClient as ConstClient
+    from client.Back_file import SampleClient as BackClient
     from client.yaduya_agent import PlayerReinforce as PlayerReinforce
-    from client.yaduya_agent import PlayerLogi as PlayerLogi
+    from client.MJ import ClientMJ as ClientMJ
+    from client.Kuron import Kuron as ClientKuron
+    from client.hamao_ni_melomelo import HamaoNiMeloMelo as ClientHamaoNiMeloMelo   
+    from client.akazudayo import BaseLine as ClientBaseLine
     
     predefs = [
         
-        [PlayerLogi(player_name="PreAI1", is_ai=True), "PreAI1"],
-        [PlusOneClient(player_name="PreAI2", is_ai=True), "PreAI2"],
-        [PlayerLogi(player_name="PreAI3", is_ai=True), "PreAI3"],
-        [PlusOneClient(player_name="PreAI4", is_ai=True), "PreAI4"],
-        [PlayerLogi(player_name="PreAI5", is_ai=True), "PreAI5"],
-        [PlayerReinforce(player_name="PreAI6", is_ai=True), "PreAI6"],
+        [BackClient(player_name="PreAI1", is_ai=True), "PreAI1"],
+        [PlayerReinforce(player_name="PreAI2", is_ai=True), "PreAI2"],
+        [ClientMJ(player_name="PreAI3", is_ai=True), "PreAI3"],
+        [ClientKuron(player_name="PreAI4", is_ai=True), "PreAI4"],
+        [ClientHamaoNiMeloMelo(player_name="PreAI5", is_ai=True), "PreAI5"],
+        [ClientBaseLine(player_name="PreAI6", is_ai=True), "PreAI6"],
     ]
     
     arena = Arena(total_matches=5, predefined_clients=predefs)
---
+++ client/akazudayo.py
@@ -0,0 +1,9 @@
+from .not_websocket_client import Client
+class BaseLine(Client):
+    def AI_player_action(self,sum,actions):
+        current_action = actions[1] - 1 
+        if sum + 5 <= current_action:
+            action = actions[0]
+        else:
+            action = actions[1]
+        return action
